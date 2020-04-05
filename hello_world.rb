60ed03adddd0252f27f63a1fe920b06e62ce3f1c
vonnenaut <danielashcom@gmail.com>
2020-04-05 17:35:01 +0700

    u/d hello_world and added advice.rb

--- hello_world.rb
+++ hello_world.rb
@@ -1,6 +1,20 @@
 # hello_world.rb
+require_relative 'advice'  
+
 class HelloWorld
   def call(env)
-    ['200', {'Content-Type' => 'text/plain'}, ['Hello World!']]
+    case env['REQUEST_PATH']
+    when '/'
+      ['200', {"Content-Type" => 'text/plain'}, ["Hello World!"]]
+    when '/advice'
+      piece_of_advice = Advice.new.generate
+      ['200', {'Content-Type' => 'text/plain'}, [piece_of_advice]]
+    else
+      [
+        '404',
+        {"Content-Type" => 'text/plain', "Cntent-Length" => '13'},
+        ["404 Not Found"]
+      ]
+    end
   end
 end
