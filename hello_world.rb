8a57d8cfb78da2b34b39bcedf94495f8c313cec9
vonnenaut <danielashcom@gmail.com>
2020-04-06 09:11:23 +0700

    u/d hello_world.rb

--- hello_world.rb
+++ hello_world.rb
@@ -5,15 +5,23 @@ class HelloWorld
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      ['200', {"Content-Type" => 'text/plain'}, ["Hello World!"]]
+      [
+        '200',
+        {"Content-Type" => 'text/html'},
+        ["<html><body>Hello World!</body></html>"]
+      ]
     when '/advice'
       piece_of_advice = Advice.new.generate
-      ['200', {'Content-Type' => 'text/plain'}, [piece_of_advice]]
+      [
+        '200',
+        {'Content-Type' => 'text/html'},
+        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+      ]
     else
       [
         '404',
-        {"Content-Type" => 'text/plain', "Cntent-Length" => '13'},
-        ["404 Not Found"]
+        {"Content-Type" => 'text/html', "Cntent-Length" => '13'},
+      ["<html><body><h4>404 Not Found</h4></body></html>"]
       ]
     end
   end
